d5fcda177e1d8c17f15cd7a2ed36b5cd509b11ee
Diego Pagini <dq@qbkconsulting.com>
2021-03-16 06:57:12 -0300

    componentes creados y rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CargaComponent } from './components/carga/carga.component';
+import { FotosComponent } from './components/fotos/fotos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'fotos', component: FotosComponent },
+  { path: 'carga', component: CargaComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'fotos' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FotosComponent } from './components/fotos/fotos.component';
+import { CargaComponent } from './components/carga/carga.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FotosComponent,
+    CargaComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,

29e50e19bd1583061de0ef7c8ce26072b22a8020
Diego Pagini <dq@qbkconsulting.com>
2021-03-16 07:46:54 -0300

    validaciones en la directiva creadas

--- src/app/directives/ng-drop-files.directive.ts
+++ src/app/directives/ng-drop-files.directive.ts
@@ -6,11 +6,13 @@ import {
   Input,
   Output,
 } from '@angular/core';
+import { FileItem } from '../models/file-item';
 
 @Directive({
   selector: '[appNgDropFiles]',
 })
 export class NgDropFilesDirective {
+  @Input() archivos: FileItem[] = [];
   @Output() mouseSobre: EventEmitter<boolean> = new EventEmitter();
 
   constructor() {}
@@ -23,5 +25,38 @@ export class NgDropFilesDirective {
   @HostListener('dragleave', ['$event'])
   public onDragLeave(event: any) {
     this.mouseSobre.emit(false);
+  }
+
+  // Validaciones
+  private _archivoPuedeSerCargado(archivo: File): boolean {
+    if (
+      !this._archivoYaFueDroppeado(archivo.name) &&
+      this._esImagen(archivo.type)
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  private _prevenirDetener(event) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  private _archivoYaFueDroppeado(nombreArchivo: string): boolean {
+    for (const archivo of this.archivos) {
+      if (archivo.nombreArchivo == nombreArchivo) {
+        console.log('El archivo ' + nombreArchivo + ' ya esta agregado');
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private _esImagen(tipoArchivo: string): boolean {
+    return tipoArchivo === '' || tipoArchivo === undefined
+      ? false
+      : tipoArchivo.startsWith('image');
   }
 }
